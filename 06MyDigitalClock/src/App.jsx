8bb4215289fd75f3834371eda80c195496aafb5e
Farooquekk <22sw040@students.muet.edu.pk>
2025-02-03 04:14:04 -0700

    Creating a Digital Clock in React.

---
+++ 06MyDigitalClock/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import DigitalClock from './components/DigitalClock'
+
+function App() {
+ 
+
+  return (
+    <>
+      <DigitalClock/>
+    </>
+  )
+}
+
+export default App
